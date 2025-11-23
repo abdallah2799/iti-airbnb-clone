4823ef94522b9b44e6c8a44b2b4e6cc57fa47bb3
Karem Saeed <95093124+karemsaeed20020@users.noreply.github.com>
2025-11-23 19:52:49 +0200

    Modify User Profile Mapping

--- src/backend/AirbnbClone/Application/Helpers/MappingProfile.cs
+++ src/backend/AirbnbClone/Application/Helpers/MappingProfile.cs
@@ -8,19 +8,12 @@
 
 namespace AirbnbClone.Application.Helpers
 {
-    public class MappingProfile : Profile
+    public class MappingProfile
     {
         public MappingProfile()
         {
            
-            CreateMap<Review, ReviewDto>()
-                 .ForMember(dest => dest.GuestName,
-                     opt => opt.MapFrom(src => src.Guest != null ? src.Guest.FullName ?? src.Guest.UserName : "Unknown User"))
-                 .ForMember(dest => dest.GuestAvatar,
-                     opt => opt.MapFrom(src => src.Guest != null ? src.Guest.ProfilePictureUrl ?? string.Empty : string.Empty));
-
-            CreateMap<CreateReviewDto, Review>();
-            CreateMap<ApplicationUser, ProfileDto>();
+            
         }
     }
     }
